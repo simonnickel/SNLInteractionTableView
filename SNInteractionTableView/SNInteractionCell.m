cf1f07d71a89e2da084a6c7e74741a3cfeb4c17d
Simon Nickel <simonnickel@googlemail.com>
2014-02-07 18:34:09 +0100

    add: wrap cell.contentView with containerView and maintain constraints

--- SNInteractionTableView/SNInteractionCell.m
+++ SNInteractionTableView/SNInteractionCell.m
@@ -29,6 +29,28 @@
      * setup container view and wrap it around the subviews of contentView
      */
     self.container = [[UIView alloc] init];
+    NSMutableArray *newconstraints = [[NSMutableArray alloc] init];
+    
+    // maintain constraints
+    NSArray *constraints = self.contentView.constraints;
+    for (NSLayoutConstraint *constraint in constraints) {
+        // replace constraints to contentView with container
+        if (constraint.firstItem == self.contentView ||
+            constraint.secondItem == self.contentView
+        ) {
+            NSLayoutConstraint *newconst;
+            if (constraint.firstItem == self.contentView)
+                newconst = [NSLayoutConstraint constraintWithItem:self.container attribute:constraint.firstAttribute relatedBy:constraint.relation toItem:constraint.secondItem attribute:constraint.secondAttribute multiplier:constraint.multiplier constant:constraint.constant];
+            else newconst = [NSLayoutConstraint constraintWithItem:constraint.firstItem attribute:constraint.firstAttribute relatedBy:constraint.relation toItem:self.container attribute:constraint.secondAttribute multiplier:constraint.multiplier constant:constraint.constant];
+            [newconstraints addObject:newconst];
+            [self.contentView removeConstraint:constraint];
+        }
+        // move other constraints into container
+        else {
+            [newconstraints addObject:constraint];
+            [self.contentView removeConstraint:constraint];
+        }
+    }
     
     // remove subviews from contentView and add them to container
     NSArray *contentViews = self.contentView.subviews;
@@ -36,18 +58,20 @@
         [view removeFromSuperview];
         [self.container addSubview:view];
     }
+    [self.container addConstraints:newconstraints];
     [self.contentView addSubview:self.container];
     
     // layout container to fill contentView
     [self.container setTranslatesAutoresizingMaskIntoConstraints:NO];
-    NSLayoutConstraint *top = [NSLayoutConstraint constraintWithItem:self.container attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.contentView attribute:NSLayoutAttributeTop multiplier:1.0f constant:0.f];
-    NSLayoutConstraint *bottom = [NSLayoutConstraint constraintWithItem:self.container attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.contentView attribute:NSLayoutAttributeBottom multiplier:1.0f constant:0.f];
-    NSLayoutConstraint *left = [NSLayoutConstraint constraintWithItem:self.container attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.contentView attribute:NSLayoutAttributeLeft multiplier:1.0f constant:0.f];
-    NSLayoutConstraint *right = [NSLayoutConstraint constraintWithItem:self.container attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self.contentView attribute:NSLayoutAttributeRight multiplier:1.0f constant:0.f];
+    NSLayoutConstraint *top = [NSLayoutConstraint constraintWithItem:self.container attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.contentView attribute:NSLayoutAttributeTop multiplier:1.0f constant:10.f];
+    NSLayoutConstraint *bottom = [NSLayoutConstraint constraintWithItem:self.container attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.contentView attribute:NSLayoutAttributeBottom multiplier:1.0f constant:-10.f];
+    NSLayoutConstraint *left = [NSLayoutConstraint constraintWithItem:self.container attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.contentView attribute:NSLayoutAttributeLeft multiplier:1.0f constant:10.f];
+    NSLayoutConstraint *right = [NSLayoutConstraint constraintWithItem:self.container attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self.contentView attribute:NSLayoutAttributeRight multiplier:1.0f constant:-50.f];
     [self.contentView addConstraints:@[top, bottom, left, right]];
     
+    
+    
     [self.container setBackgroundColor:[UIColor greenColor]];
-
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated

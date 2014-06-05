2432729ac4673d8cc518d19e22b80ef6c1613aa6
Simon Nickel <simonnickel@googlemail.com>
2014-06-05 12:33:15 +0200

    cleanup example

--- InteractionTableViewExample/InteractionTableViewExample/SNLExampleTableViewController.m
+++ InteractionTableViewExample/InteractionTableViewExample/SNLExampleTableViewController.m
@@ -9,15 +9,6 @@
 
 @implementation SNLExampleTableViewController
 
-- (id)initWithStyle:(UITableViewStyle)style
-{
-    self = [super initWithStyle:style];
-    if (self) {
-        
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -32,12 +23,6 @@
  
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 #pragma mark - Table view data source
@@ -140,6 +125,7 @@
 
 
 
+#pragma mark - Interaction
 
 - (void)buttonA:(id)sender {
     NSLog(@"A");
@@ -153,76 +139,5 @@
     [self performSegueWithIdentifier:@"detail" sender:self];
 }
 
-- (void)prepareForSegue:(UIStoryboardPopoverSegue *)segue sender:(id)sender {
-    /*
-    NSIndexPath *indexPath = [self.tableView indexPathForCell:self.activeCell];
-    
-    if ([[segue identifier] isEqualToString:@"detail"]) {
-        ItemDetailViewController *detailVC = [segue destinationViewController];
-        
-        id object = [self.itemList objectAtIndex:indexPath.row];
-        
-        [detailVC setObject:object];
-    }
-     */
-}
-- (BOOL)shouldPerformSegueWithIdentifier:(NSString *)identifier sender:(id)sender {
-    if ([identifier isEqualToString:@"detail" ]) {
-        return YES;
-    }
-    return NO;
-}
-
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a story board-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-
- */
 
 @end
